5238599e6bc7246a06fa1cca7b18779dadea0ef4
Anno59 <372025284@qq.com>
2017-02-18 16:01:14 +0800

    test

--- configs/configs.php
+++ configs/configs.php
@@ -6,10 +6,8 @@
  * Time: 17:54
  */
 require_once '../include.php';
-
 define('HOST',"localhost");
 define('PWD','123');
-
 define('USERNAME','root');
 define('DBNAME','shopimooc');
 define('DBCHARSET','utf8');
